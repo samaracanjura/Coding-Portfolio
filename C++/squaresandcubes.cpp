01fcd75dab6d9a68ed362c15906d8fc75001f8df
Samara Canjura <82206324+samaracanjura@users.noreply.github.com>
2021-04-09 22:30:13 -0700

    Create squaresandcubes.cpp
    This program calculates the squares and cubes of the numbers from 1 - 10 and uses tabs to print a table of values.

---
+++ C++/squaresandcubes.cpp
@@ -0,0 +1,22 @@
+//Samara Canjura
+//Student Number: 1239028
+//Assignment 1
+//This program calculates the squares and cubes of the numbers from 1 - 10 and uses tabs to print a table of values. 
+#include <iostream>
+
+int main()
+   {
+      cout << "number" << "\t\t" << "square" << "\t\t" << "cube" <<endl;
+      cout << "------" << "\t\t" << "------" << "\t\t" << "------" <<endl; 
+      cout << 1 << "\t\t" << 1*1 << "\t\t" << 1*1*1 <<endl;
+      cout << 2 << "\t\t" << 2*2 << "\t\t" << 2*2*2 <<endl;
+      cout << 3 << "\t\t" << 3*3 << "\t\t" << 3*3*3 <<endl;
+      cout << 4 << "\t\t" << 4*4 << "\t\t" << 4*4*4 <<endl;
+      cout << 5 << "\t\t" << 5*5 << "\t\t" << 5*5*5 <<endl;
+      cout << 6 << "\t\t" << 6*6 << "\t\t" << 6*6*6 <<endl; 
+      cout << 7 << "\t\t" << 7*7 << "\t\t" << 7*7*7 <<endl;
+      cout << 8 << "\t\t" << 8*8 << "\t\t" << 8*8*8 <<endl; 
+      cout << 9 << "\t\t" << 9*9 << "\t\t" << 9*9*9 <<endl;
+      cout << 10 << "\t\t" << 10*10 << "\t\t" << 10*10*10 << endl; 
+   
+   }
